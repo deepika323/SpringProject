c37914bbcd3d5c6d1183567e03f17c91a9c81a34
Deepika Chhabra <dc4n999@gmail.com>
2017-03-15 17:06:57 +0530

    BillServlet Change

--- HospitalManagementSystem/src/controller/patientServlet/BillServlet.java
+++ HospitalManagementSystem/src/controller/patientServlet/BillServlet.java
@@ -17,52 +17,55 @@ import controller.loginSignUp.LogInController;
 import model.bl.PersonBusinessLogic;
 
 
-public class BillServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class BillServlet{
 	
-	private static Logger logger=Logger.getLogger(BillServlet.class);
-
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//code to get bill using personId.
-		
-		HttpSession session=request.getSession();
-		String personId =(String)session.getAttribute("personId");
-		
-		
-		
-		
-		PersonBusinessLogic pb = new PersonBusinessLogic();
-		try { 
-			if(pb.listMyBill(personId)!=null){
-				
-				request.setAttribute("billList", pb.listMyBill(personId));
-				
-				BasicConfigurator.configure();
-		 	    logger.info("Bill viewed by Patient!!");
-				
-				RequestDispatcher rd = getServletContext().getRequestDispatcher("/bill.jsp");
-			    rd.forward(request, response);
-			}//Add Bill jsp and recheck logic.
-			else 
-				{
-					String message="Bill not found";
-					session.setAttribute("message", message);
-					response.sendRedirect("ErrorPage.jsp");
-				}
-		} catch (ClassNotFoundException| IOException e) {
-			// TODO Auto-generated catch block 
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block 
-			e.printStackTrace();
-		} 
-	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
+//public class BillServlet extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//	
+//	private static Logger logger=Logger.getLogger(BillServlet.class);
+//
+//	
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		//code to get bill using personId.
+//		
+//		HttpSession session=request.getSession();
+//		String personId =(String)session.getAttribute("personId");
+//		
+//		
+//		
+//		
+//		PersonBusinessLogic pb = new PersonBusinessLogic();
+//		try { 
+//			if(pb.listMyBill(personId)!=null){
+//				
+//				request.setAttribute("billList", pb.listMyBill(personId));
+//				
+//				BasicConfigurator.configure();
+//		 	    logger.info("Bill viewed by Patient!!");
+//				
+//				RequestDispatcher rd = getServletContext().getRequestDispatcher("/bill.jsp");
+//			    rd.forward(request, response);
+//			}//Add Bill jsp and recheck logic.
+//			else 
+//				{
+//					String message="Bill not found";
+//					session.setAttribute("message", message);
+//					response.sendRedirect("ErrorPage.jsp");
+//				}
+//		} catch (ClassNotFoundException| IOException e) {
+//			// TODO Auto-generated catch block 
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block 
+//			e.printStackTrace();
+//		} 
+//	}
+//
+//	
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+//
+//}
